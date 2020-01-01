01649aedcebfbc5de47d28f420e9bc8a474a6c4f
Andrey Zhidenkov <pensnarik@gmail.com>
2020-01-01 22:08:05 +0100

    Faster path calculation

--- db/schema/web/functions/product_path_get.sql
+++ db/schema/web/functions/product_path_get.sql
@@ -13,3 +13,40 @@ function web.product_path_get(aid integer) returns integer[] as $$
 $$ language sql security definer stable;
 
 alter function web.product_path_get(integer) owner to shop;
+
+
+create or replace
+function web.product_path_get_fast(aid integer) returns integer[] as $$
+declare
+  result integer[];
+  vid integer; vparent_id integer;
+begin
+  select category_id into vid from shop.product_category where product_id = aid limit 1;
+
+  if vid is null then
+    return null;
+  end if;
+
+  result := array[vid];
+
+  loop
+    select parent_id, id
+      into vparent_id, vid
+      from shop.category c
+     where c.id = vid;
+
+    if vparent_id is not null then
+      result := array[vparent_id] || result;
+      vid := vparent_id;
+    else
+      exit;
+    end if;
+  end loop;
+
+  return result;
+end;
+$$ language plpgsql;
+
+create trigger tidu_product_category_fast
+  AFTER INSERT OR DELETE OR UPDATE ON product_category
+  FOR EACH ROW EXECUTE PROCEDURE tidu_product_category_fast();
